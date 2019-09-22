0474723358d513c5d57d8d332072911ef290be86
ArminCoder <armincoding@gmail.com>
2019-09-22 16:38:44 +0200

    added a transition on the hover

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,9 +2,9 @@
   <section class="home">
     <b-container>
       <b-row>
-        <b-col class="link-box" v-for="(link, index) in links" :key="link.value" :sm="index === 4 ? 12 : 6" :md="index > 2 ? 6 : 4">
+        <b-col @mouseover='hoverEffect' @mouseleave="leaveHoverEffect" class="link-box" v-for="(link, index) in links" :key="link.value" :sm="index === 4 ? 12 : 6" :md="index > 2 ? 6 : 4">
           <router-link :to="link.path">
-            <article :class="['link-box__container link-'+index , link.value]">
+            <article  :class="['link-box__container link-'+index , link.value, { overlay: isHovered }]">
               <div class="link-box__container__footer">{{ link.name }}</div>
             </article>
           </router-link>
@@ -26,12 +26,22 @@ export default {
   data() {
     return {
       links: navigation,
+      isHovered: false
     }
   },
   methods: {
     activate(path) {
       console.log(path);
     },
+    hoverEffect(e) {
+      this.isHovered = true;
+      this.$nextTick(() => {
+       e.target.classList.remove('overlay');
+      })
+    },
+    leaveHoverEffect(e) {
+        this.isHovered = false;
+    }
   }
 }
 </script>
@@ -72,6 +82,16 @@ export default {
         color: #fff;
         text-transform: uppercase;
         font-weight: 700;
+        pointer-events: none;
+      }
+
+      .overlay {
+         -webkit-filter: brightness(50%);
+         transition: all 500ms;
+      }
+
+      .noOverlay {
+        filter: none !important;
       }
 
       .procjene { 
